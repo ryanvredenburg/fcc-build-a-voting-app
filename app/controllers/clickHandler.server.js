26abacd861394d159b6e7f28f16988cbd505f295
System <none>
2017-08-26 11:19:36 +0000

    Added standard and performed standard --fix

--- app/controllers/clickHandler.server.js
+++ app/controllers/clickHandler.server.js
@@ -1,41 +1,39 @@
-'use strict';
+'use strict'
 
-var Users = require('../models/users.js');
+var Users = require('../models/users.js')
 
 function ClickHandler () {
-
-	this.getClicks = function (req, res) {
-		Users
+  this.getClicks = function (req, res) {
+    Users
 			.findOne({ 'github.id': req.user.github.id }, { '_id': false })
 			.exec(function (err, result) {
-				if (err) { throw err; }
+  if (err) { throw err }
 
-				res.json(result.nbrClicks);
-			});
-	};
+  res.json(result.nbrClicks)
+})
+  }
 
-	this.addClick = function (req, res) {
-		Users
+  this.addClick = function (req, res) {
+    Users
 			.findOneAndUpdate({ 'github.id': req.user.github.id }, { $inc: { 'nbrClicks.clicks': 1 } })
 			.exec(function (err, result) {
-					if (err) { throw err; }
+  if (err) { throw err }
 
-					res.json(result.nbrClicks);
-				}
-			);
-	};
+  res.json(result.nbrClicks)
+}
+			)
+  }
 
-	this.resetClicks = function (req, res) {
-		Users
+  this.resetClicks = function (req, res) {
+    Users
 			.findOneAndUpdate({ 'github.id': req.user.github.id }, { 'nbrClicks.clicks': 0 })
 			.exec(function (err, result) {
-					if (err) { throw err; }
+  if (err) { throw err }
 
-					res.json(result.nbrClicks);
-				}
-			);
-	};
-
+  res.json(result.nbrClicks)
+}
+			)
+  }
 }
 
-module.exports = ClickHandler;
+module.exports = ClickHandler
--- app/models/polls.js
+++ app/models/polls.js
@@ -1,19 +1,18 @@
-'use strict';
+'use strict'
 
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
 
 var Poll = new Schema({
-	creator_id: String,
-	question: String,
-	votes: {type:Number, default: 0},
-	options: [
+  creator_id: String,
+  question: String,
+  votes: {type: Number, default: 0},
+  options: [
     {
       name: String,
-      votes: {type:Number, default: 0}
+      votes: {type: Number, default: 0}
     }
   ]
-});
+})
 
-module.exports = mongoose.model('Poll', Poll);
+module.exports = mongoose.model('Poll', Poll)
